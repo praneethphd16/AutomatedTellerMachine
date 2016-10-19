f529b0832d447fb8a29246c716ed17293182e84d
Raja Kumar <Raja Kumar@DESKTOP-0Q4VGP1>
2016-10-19 16:37:03 +0530

    Add project files.

---
+++ AutomatedTellerMachine/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutomatedTellerMachine.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Having Trouble ? Send us a message";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(string message) {
+            ViewBag.TheMessage = "Thanks we got your details";
+            return View();
+        }
+
+        public ActionResult Foo() {
+
+
+            return View("Contact");
+        }
+         
+        public ActionResult Serial(string letterCase) {
+            var serial = "ASPNETMVC5ATM1";
+            if (letterCase == "lower") {
+
+                return Content(serial.ToLower());
+            }
+
+            return Json(new { name = "serial", value = serial }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
