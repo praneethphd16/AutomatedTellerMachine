ea05fa909b079a1256b0ec0a2c9cd6100f143a77
praneeth <praneeth@DESKTOP-8LM5FPA>
2016-10-20 07:00:40 +0530

    Checking Account added

---
+++ AutomatedTellerMachine/Models/CheckingAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AutomatedTellerMachine.Models
+{
+    public class CheckingAccount
+    {
+
+        public int Id { get; set; }
+        [Display(Name="Account #")]
+        public string AccountNumber { get; set; }
+
+        [Display(Name = "First Name")]
+
+        public string FirstName { get; set; }
+
+        
+        [Display(Name = "Name")]
+        public string Name { get {
+
+                return string.Format("{0},{1}",this.FirstName,this.LastName);
+
+            } }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [DataType(DataType.Currency)]
+        public decimal Balance { get; set; }
+    }
+}
